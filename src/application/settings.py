a254bf3915b0cfc6f63dc13330abcc521cfd0034
lesina <lesha_lesovoy@mail.ru>
2019-08-06 21:15:02 +0300

    sommeshit :poop:

--- src/application/settings.py
+++ src/application/settings.py
@@ -27,9 +27,9 @@ config.read(os.path.join(BASE_DIR, '../django.conf'))
 SECRET_KEY = config.get('main', 'SECRET')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 # Application definition
 
@@ -123,7 +123,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(PROJECT_DIR, 'collected_static')
+STATIC_ROOT = os.path.join(PROJECT_DIR, 'static/')
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
 MEDIA_URL = '/media/'
