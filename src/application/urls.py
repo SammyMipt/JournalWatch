5c230be1ab6ee5d8d22fcf9814ab84c0208e15a4
lesina <lesha_lesovoy@mail.ru>
2019-08-07 20:16:48 +0300

    added slashes

--- src/application/urls.py
+++ src/application/urls.py
@@ -17,9 +17,13 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from .settings import DEBUG
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path(r'', include('article.urls'), name="article"),
     path(r'accounts/', include('core.urls'), name="core"),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
+
+if DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
